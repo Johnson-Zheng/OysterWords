44d5d634c9ff6003b312247840a8b7c0994a0b6a
十一 <50663196+sususnow@users.noreply.github.com>
2020-12-09 17:26:16 +0800

    个人信息页面（绑定数据+注销跳转页面）
    1、修改了个人信息页面的部分UI，增加了“修改密码”
    2、实现了“修改密码”“修改昵称”“注销”功能

--- oyster_words/src/global/interfaceURL.js
+++ oyster_words/src/global/interfaceURL.js
@@ -1,3 +1,6 @@
 //用户登录
 export const login = "http://39.107.84.0:8600/login";
 export const register = "http://39.107.84.0:8600/register";
+export const resetPassword = "http://39.107.84.0:8600/resetPassword";
+export const changeUsername = "http://39.107.84.0:8600/changeUsername";
+export const changeFace = "http://39.107.84.0:8600/changeFace";
